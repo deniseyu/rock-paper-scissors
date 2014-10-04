62407fedf8ba39b8470120b1d143e3ab6470f805
Denise <yu.denise.d@gmail.com>
2014-10-04 21:51:03 +0100

    Two player mode working

--- lib/rps.rb
+++ lib/rps.rb
@@ -29,6 +29,7 @@ class RPS < Sinatra::Base
 
   get '/game' do 
     player = session[:player]
+    friend = session[:friend]
     erb :game
   end
 
@@ -36,9 +37,9 @@ class RPS < Sinatra::Base
     player = session[:player]
     @name = player.name 
     @mode = params[:mode].to_s
+    session[:mode] = @mode
     @choice = player.choice
     @opp_choice = session[:game_choice]
-    @outcome = session[:outcome]
     erb :game
   end
 
@@ -51,6 +52,33 @@ class RPS < Sinatra::Base
     redirect '/outcome'
   end
 
+  post '/choice_friend_mode' do 
+    player = session[:player]
+    friend = session[:friend]
+    player.choice = session[:choice]
+    friend.choice = params[:friend_choice].to_s
+    session[:outcome] = GAME.outcome(player.choice, friend.choice)
+    redirect '/outcome_friend_mode'
+  end
+
+  get '/player_one_choice' do
+    @player = session[:player]
+    erb :player_one_choice    
+  end 
+
+  post '/player_one_choice' do 
+    @player = session[:player]
+    @player.choice = params[:choice].to_s
+    session[:choice] = @player.choice 
+    @friend = session[:friend]
+    redirect '/player_two_choice' 
+  end
+
+  get '/player_two_choice' do 
+    @friend = session[:friend]
+    erb :player_two_choice 
+  end 
+
   get '/outcome' do
     player = session[:player]
     @choice = player.choice
@@ -58,6 +86,31 @@ class RPS < Sinatra::Base
     @outcome = session[:outcome]
     erb :outcome
   end
+
+  get '/outcome_friend_mode' do
+    @player = session[:player]
+    @choice = @player.choice
+    @friend = session[:friend]
+    @friend_choice = @friend.choice 
+    @outcome = session[:outcome]
+    erb :outcome_friend_mode
+  end
+
+  get '/vs_friend' do 
+    erb :vs_friend
+  end
+
+  post '/vs_friend' do 
+    @player = session[:player]
+    @player_choice = @player.choice 
+    session[:friend] = Player.new 
+    @friend = session[:friend]
+    @friend.name = params[:friend_name]
+    @friend_choice = @friend.choice 
+    erb :vs_friend
+  end
+
+
 
 
   # start the server if ruby file executed directly
